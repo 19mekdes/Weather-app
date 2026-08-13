80c3c84b0923c3f1bc2da1dcb3aa745e3b6e5064
mekdes wale <mekdesw60@gmail.com>
2026-08-13 07:55:08 +0300

    update

--- lib/widgets/info_item.dart
+++ lib/widgets/info_item.dart
@@ -4,21 +4,13 @@ class InfoItem extends StatelessWidget {
   final IconData icon;
   final String value;
 
-  const InfoItem({
-    super.key,
-    required this.icon,
-    required this.value,
-  });
+  const InfoItem({super.key, required this.icon, required this.value});
 
   @override
   Widget build(BuildContext context) {
     return Column(
       children: [
-        Icon(
-          icon,
-          color: Colors.white70,
-          size: 28,
-        ),
+        Icon(icon, color: Colors.white70, size: 28),
         const SizedBox(height: 6),
         Text(
           value,
--- lib/widgets/weather_card.dart
+++ lib/widgets/weather_card.dart
@@ -1,4 +1,5 @@
 import 'package:flutter/material.dart';
+
 class WeatherCard extends StatelessWidget {
   final String temperature;
   final String description;
@@ -15,26 +16,16 @@ class WeatherCard extends StatelessWidget {
   Widget build(BuildContext context) {
     return Container(
       width: double.infinity,
-      padding: const EdgeInsets.symmetric(
-        vertical: 30,
-        horizontal: 20,
-      ),
+      padding: const EdgeInsets.symmetric(vertical: 30, horizontal: 20),
       decoration: BoxDecoration(
         // ignore: deprecated_member_use
         color: Colors.white.withOpacity(0.12),
         borderRadius: BorderRadius.circular(25),
-        border: Border.all(
-          color: Colors.white24,
-        ),
+        border: Border.all(color: Colors.white24),
       ),
       child: Column(
         children: [
-          Text(
-            weatherIcon,
-            style: const TextStyle(
-              fontSize: 80,
-            ),
-          ),
+          Text(weatherIcon, style: const TextStyle(fontSize: 80)),
           const SizedBox(height: 15),
           Text(
             temperature,
@@ -47,10 +38,7 @@ class WeatherCard extends StatelessWidget {
           const SizedBox(height: 10),
           Text(
             description,
-            style: const TextStyle(
-              color: Colors.white70,
-              fontSize: 20,
-            ),
+            style: const TextStyle(color: Colors.white70, fontSize: 20),
           ),
         ],
       ),
